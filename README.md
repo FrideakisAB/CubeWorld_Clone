12b2b5b7bc9f6e0dd23223525b7a05cce513cf70
FrideakisAB <reterdastion@gmail.com>
2021-07-10 19:00:12 +0300

    Сделана заготовка проекта с подключением Conan и библиотек Добавлено описание Дополнен .gitignore файлами проекта IDE

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # CubeWorld_Clone
+
+Проект по созданию копии **печально** известной игры Cube World.
+
+## Поддерживаемые платформы
+
+* Windows
+
+## Структура проекта
+
+* Движок (C++17 migration to 20, CMake, Conan)
+* Игра (later...)
+
+## Используемые проекты
+
+* GLM
+* GLEW
+* GLFW
+* Catch2
+* assimp
+* stb_image
+* json (nlohmann)
---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+#include <windows.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void window_focus_callback(GLFWwindow *window, int focused);
+void scroll_callback(GLFWwindow *window, double xOffset, double yOffset);
+void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
+void cursor_position_callback(GLFWwindow *window, double xPos, double yPos);
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+
+int WINAPI WinMain(HINSTANCE hThisInst, HINSTANCE hPrevInst, LPSTR str, int nWinMode)
+{
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *glfwWindow = glfwCreateWindow(1280, 720, "Cube World", nullptr, nullptr);
+    if (glfwWindow == nullptr)
+        return -2;
+
+    glfwMakeContextCurrent(glfwWindow);
+
+    glfwSetWindowSizeLimits(glfwWindow, 640, 480, GLFW_DONT_CARE, GLFW_DONT_CARE);
+
+    glfwSetKeyCallback(glfwWindow, key_callback);
+    glfwSetCursorPosCallback(glfwWindow, cursor_position_callback);
+    glfwSetMouseButtonCallback(glfwWindow, mouse_button_callback);
+    glfwSetScrollCallback(glfwWindow, scroll_callback);
+    glfwSetWindowFocusCallback(glfwWindow, window_focus_callback);
+
+    glfwSwapInterval(0);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        glfwTerminate();
+
+        return -3;
+    }
+
+    if (!GLEW_VERSION_4_4)
+    {
+        glfwTerminate();
+
+        return -4;
+    }
+
+    while (!glfwWindowShouldClose(glfwWindow))
+    {
+        glfwPollEvents();
+
+        int width, height;
+        glfwGetFramebufferSize(glfwWindow, &width, &height);
+
+        glfwSwapBuffers(glfwWindow);
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+void window_focus_callback(GLFWwindow *window, int focused)
+{
+
+}
+
+void scroll_callback(GLFWwindow *window, double xOffset, double yOffset)
+{
+
+}
+
+void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
+{
+
+}
+
+void cursor_position_callback(GLFWwindow *window, double xPos, double yPos)
+{
+
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
